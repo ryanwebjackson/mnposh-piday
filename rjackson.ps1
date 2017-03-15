a3df6853dad8818ad4abade442b3698a37a7c5d7
Ryan Jackson <ryan.jackson@cit-net.com>
2017-03-15 09:00:22 -0500

    Initial commit

---
+++ rjackson.ps1
@@ -0,0 +1,19 @@
+﻿$continue = $true
+while($continue)
+{
+
+    if ([console]::KeyAvailable)
+    {
+        echo "Exit script with Enter key";
+        $x = [System.Console]::ReadKey() 
+
+        switch ($x.key)
+        {
+            ~ { $continue = $false }
+        }
+    } 
+    else
+    {
+        sleep 1
+    }    
+}
